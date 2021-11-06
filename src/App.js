e6cdf321ed89f1a022ca309598d3cc95cb90fb26
sead <karisiksead@gmail.com>
2021-11-06 20:21:44 +0100

    icons package added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { FaBeer } from "react-icons/fa";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Dumb header test</h1>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        h1 > Dumb header test < /h1> <FaBeer / > { " " } <
+        /div>
+    );
 }
 
 export default App;
