5d3b8353dc3a62c9eb45e6c43b043c0d4b47a479
Muhamed Sadovic <muhamedsadovic41@gmail.com>
2021-11-06 20:47:34 +0100

    added toast package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const notify = () => toast("Wow so easy !");
+
   return (
     <div className="App">
       <h1>Dumb header test</h1>
+      <button onClick={notify} style={{backgroundColor: "red"}}>Notify !</button>
+      <ToastContainer />
     </div>
   );
 }
