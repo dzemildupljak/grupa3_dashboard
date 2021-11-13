b6481099ebc64b071ce0299645086783f496935b
FarisBektasevic <bektasevicfaris@gmail.com>
2021-11-13 19:49:31 +0100

    changed desing and some litle corrections

--- src/components/sidebar/sidebarElement/SidebarElement.jsx
+++ src/components/sidebar/sidebarElement/SidebarElement.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { RiArrowDropDownLine } from 'react-icons/ri'
 
 let SidebarElementStyle =
-  'w-full h-12 pl-7 pr-4 flex justify-between items-center hover:bg-blue-500 transition-all duration-300 ease-in'
+  'w-11/12 h-14 pl-8 pr-4 my-2 rounded-xl flex justify-between items-center hover:bg-purple-200 transition-all duration-300 ease-in hover:pl-12'
 
 const SidebarElement = ({ element }) => {
   const { name, dropdown } = element
@@ -13,26 +13,26 @@ const SidebarElement = ({ element }) => {
   }
 
   return (
-    <>
+    <div className="w-full flex flex-col items-center">
       <div className={SidebarElementStyle} onClick={showDropdownElements}>
         <h3 className="text-2xl">{name}</h3>
         {dropdown && <RiArrowDropDownLine className="text-3xl" />}
       </div>
       {dropdown && showDropdown && (
-        <div>
+        <div className="w-11/12 flex flex-col items-center">
           {dropdown.map((dropdownElement, index) => {
             return (
               <div
                 key={index}
-                className="h-12 w-full pl-14 flex flex-col justify-center hover:bg-blue-500 transition-all duration-300 ease-in"
+                className="h-12 w-full pl-14 my-1 flex flex-col justify-center transition-all duration-300 ease-in hover:bg-purple-200 hover:pl-20 rounded-xl"
               >
-                {dropdownElement}
+                {dropdownElement.name}
               </div>
             )
           })}
         </div>
       )}
-    </>
+    </div>
   )
 }
 
